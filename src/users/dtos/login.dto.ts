4361101e105ab9d0d0fb8013913509867af339aa
Harish Gopalakrishnan <vishnuharish12@gmail.com>
2024-07-02 01:49:46 +0530

    Added Swagger support for login and sign up services

--- src/users/dtos/login.dto.ts
+++ src/users/dtos/login.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
 IsEmail, IsString
 } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsString()
+  @ApiProperty()
   password: string;
 }
